6f2a7ac49c0953e7537ddf1257d86702d369b9e5
Aaronk7 <67217076+Aaronk7@users.noreply.github.com>
2021-11-06 21:56:32 -0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,27 @@
+$("#searchbar").keyup(function(){
+    var text = $(this).val();
 
+    $.ajax({
+        url: "/",
+        type: "get",
+        data: {jsdata: text},
+        success: function(response) {
+            $("#idk").html(response);
+        },
+        error: function(xhr) {
+
+        }});
+});
+
+function hideRow(id) {
+    var row = document.getElementById(id);
+    row.style.display = 'none';
+}
+
+function hide() {
+    var checkboxes = document.getElementsByName('checkboxes');
+    for (let i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].checked)
+            hideRow(checkboxes[i].id)
+    }
+}
