563086e100d6920aa4318b05d3c4973a70357638
Aaronk7 <67217076+Aaronk7@users.noreply.github.com>
2021-12-11 19:48:35 -0800

    added detailed comments

--- MergeSearch/static/script.js
+++ MergeSearch/static/script.js
@@ -1,106 +1,147 @@
 $(document).ready(function() {
-    //var checked = $('#'+id).is(':checked');
-    //console.log(checked);
-    var count = 1;
-    var tables = [];
-    tables.push($('#results_table').DataTable());
+
+    var tables = []; // array of all datatable objects currently on the page
+    tables.push($('#results_table').DataTable()); // main datatable lives in index 0
+    var count = 1; // number of datatables on the page
     
-    //tables.push(dataTable1);
+    /* pulls in the contents of the main datatable as an array.
+     * this array is used to populate iterative datatables: */
     const arr = $('#results_table').DataTable().rows().data().toArray();
 
-
+    /* function that triggers when any checkbox is clicked within the
+     * #data_tables div. (handles row selection.) */
     $('#data_tables').on('click', 'input[type="checkbox"]', function() {
-        console.log('checkbox click');
-        var row_id = $(this).parent().parent();
-        var table_id = row_id.parent().parent().attr('id');
-        console.log('parent table ' +  table_id);
-    
-        $(row_id).toggleClass('selected');
+
+        /* grabs the parent "tr" element of the checkbox and
+         * toggles the "selected" class on it: */
+        var row = $(this).parent().parent();
+        $(row).toggleClass('selected');
     });
     
+    /* function that triggers when any button of class .hide_button
+     * is clicked within the #data_tables div. (handles row hiding.) */
     $('#data_tables').on('click', 'button.hide_button', function() {
-        let table_id = $(this).parent().parent().parent().parent().attr('id');
-        let list = [];
-        let tab = $('#'+table_id);
-        tab.find('.selected').each(function(i,v) {
-            list.push($(v));
-        });
-        console.log(list);
-        console.log('hide click');
+
+        /* grabs the id of the button, which is also the index of its
+         * datatable in the array of datatables: */
         let id = Number($(this).attr('id'));
-        console.log(table_id);
-        console.log(tables[id].table().node().id);
-        //tab = tables[id];
+
+        /* uses the button id to access its datatable and remove
+         * all rows of class .selected: */
         tables[id].rows('.selected').remove().draw();
     });
     
+    /* function that triggers when any button of class .show_button
+     * is clicked within the #data_tables div. (handles row showing.) */
     $('#data_tables').on('click', 'button.show_button', function() {
-        console.log('show click');
+
+        /* grabs the id of the button, which is also the index of its
+         * parent datatable in the array of datatables: */
         let id = Number($(this).attr('id'));
+
+        // grabs all rows of class .selected in the datatable:
         let selected = tables[id].rows('.selected').data().toArray();
+
+        /* clears the datatable of all contents, and then
+         * repopulates it with the selected rows: */
         tables[id].clear();
         tables[id].rows.add(selected).draw();
     });
     
+    /* function that triggers when any change is made to input of
+     * class .toggle-vis. (handles column hiding.) */
     $('input.toggle-vis').on('change', function(e) {
-        console.log('toggle column click');
-        var id = $(this).attr('id');
-        var checked = $('#'+id).is(':checked');
-        console.log(checked);
-        var num = $(this).attr('data-column');
-        var i = 0;
-        while (i < tables.length) {
-            var column = tables[i].column(num);
+
+        let id = $(this).attr('id'); // grabs id of the checkbox
+         // boolean var that tells if checkbox is checked:
+        let checked = $('#'+id).is(':checked');
+
+        /* grabs data-column attribute of the checkbox, which
+         * doubles as the correpsonding datatable column index: */
+        let col_index = $(this).attr('data-column');
+
+        // loops through the datatables:
+        for (let i = 0; i < tables.length; i++) {
+
+            /* in each table, sets the visibility status of the
+             * column at col_index to the truth value of checked: */
+            let column = tables[i].column(col_index);
             column.visible(checked);
-            tables[i].columns.adjust().draw();
-            i++;
         }
     });
 
+    /* function that triggers when button id #new_search is clicked
+     * within the #container div. (handles generating new tables.) */
     $('#container').on('click', '#new_search', function() {
-        let data = arr.concat();
+
+        let data = arr.concat(); // creates a copy of arr
+        // shows the merge button when at least two tables exist:
         document.getElementById('merge').style.display = '';
+        // the new table's id is 'table_' + the current table count:
         let new_id = "table_" + String(count);
-        let tab_html = "<table class='display nowrap dataTable' width='100%' id='" + new_id + "'><thead><tr><th class='hide_th'><button id=" + count + " type='button' class='show_button'>Show</button><button id=" + count + " type='button' class='hide_button'>Hide</button></th><th>Year-Semester</th><th>Class</th><th>Team#</th><th>Team Name</th><th>Project Title</th><th>Organization</th><th>Industry</th><th>Abstract</th><th>Student Names</th></tr></thead><tbody class='dt-body-left'></tbody></table>";
+
+        /* creates the html for a new blank table with only the
+         * necessary details, including a hide and show buttons: */
+        let tab_html = "<table class='display nowrap dataTable'" + 
+            "width='100%' id='" + new_id + "'><thead><tr>" +
+            "<th class='hide_th'><button id=" + count +
+            " type='button' class='show_button'>Show</button>" +
+            "<button id=" + count + " type='button' class='hide_button'>" +
+            "Hide</button></th><th>Year-Semester</th><th>Class</th>" +
+            "<th>Team#</th><th>Team Name</th><th>Project Title</th>" +
+            "<th>Organization</th><th>Industry</th><th>Abstract</th>" +
+            "<th>Student Names</th></tr></thead>" +
+            "<tbody class='dt-body-left'></tbody></table>";
+
+        // appends the html to the #iterative_tables div:
         document.getElementById('iterative_tables').innerHTML += tab_html;
+        // makes the table into a datatable and pushes it to the array:
         tables.push($('#'+new_id).DataTable());
+        // populates the datatable with the copy of arr:
         tables[count].rows.add(data);
+        // adjusts and shows table on page:
         tables[count].columns.adjust().draw();
-        //tables.push(tab);
-        let list = [];
-        let div = $('#iterative_tables');
-        div.find('table').each(function(i,v) {
-            list.push($(v));
-        });
-        console.log(list);
-        console.log(list[count-1] === tables[count]);
-        count++;
+        count++; // increments the number of tables on the page
     });
     
+    /* function that triggers when button id #merge is clicked.
+     * (handles merging tables.) */
     $('#merge').click(function() {
+
+        // hides the merge button when only one table is on the page:
         document.getElementById('merge').style.display = 'none';
+
+        /* "merged" is the array that will store all merged data.
+         * it starts off with a copy of the data from the main datatable: */
         var merged = tables[0].rows({filter:'applied'}).data().toArray();
     
+        /* loop continues until only the main datatable remains
+         * in the array of datatables: */
         while (tables.length > 1) {
+
+            // pops a datatable from the array and gets a copy of its data:
             var table  = tables.pop().rows({filter:'applied'}).data().toArray();
     
+            // loops through the rows of the popped table:
             for (let i = 0; i < table.length; i++) {
-                merged.push(table[i]);
+
+                merged.push(table[i]); // pushes each row onto "merged"
             }
         }
-    
-        // removes duplicates
-        // decides what a dupe is by comparing first 3 columns
+
+        /* removes duplicate rows from "merged"; decides what a duplicate
+         * is by comparing the values of the first 3 columns: */
         merged = merged.filter((v,i,a)=>a.findIndex(t=>(t[1] === v[1] && t[2] === v[2] && t[3] === v[3])) === i);
     
-        tables[0].clear();
-        tables[0].rows.add(merged);
-        tables[0].search('');
-        tables[0].columns.adjust().draw();
-        //tables[0] = dataTable1;
+        tables[0].clear(); // clears all data from main table
+        tables[0].rows.add(merged); // repopulates it with "merged"
+        tables[0].search(''); // clears the search bar
+        tables[0].columns.adjust().draw(); // adjusts and shows table on page
+
+        // gets the #iterative_tables div:
         var div = document.getElementById('iterative_tables');
-        div.innerHTML = '';
-        count = 1;
+        div.innerHTML = ''; // clears the old tables from the div
+        count = 1; // resets the number of tables back to 1
     });
 });
 
